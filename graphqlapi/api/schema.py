55b1499ea288abe9a7ec3424b64b2932c9417420
guindosaros <47152090+guindosaros@users.noreply.github.com>
2019-10-19 15:04:53 +0000

    commit

--- graphqlapi/api/schema.py
+++ graphqlapi/api/schema.py
@@ -1,5 +1,9 @@
 import graphene
-from graphene_django.types import DjangoObjectType
+# from graphene_django.types import DjangoObjectType
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
 from .models import *
 
 # class CategoryType(DjangoObjectType):
@@ -22,54 +26,82 @@ from .models import *
 #     def resolve_all_ingredients(self, info, **kwargs):
 #         return Ingredient.objects.select_related('category').all()
 
-class CategoryType(DjangoObjectType):
+# class CategoryType(DjangoObjectType):
+#     class Meta:
+#         model = Category
+
+
+# class IngredientType(DjangoObjectType):
+#     class Meta:
+#         model = Ingredient
+
+
+# class Query(object):
+#     category = graphene.Field(CategoryType,
+#                             id=graphene.Int(),
+#                             name=graphene.String())
+#     all_categories = graphene.List(CategoryType)
+
+
+#     ingredient = graphene.Field(IngredientType,
+#                                 id=graphene.Int(),
+#                                 name=graphene.String())
+#     all_ingredients = graphene.List(IngredientType)
+
+#     def resolve_all_categories(self, info, **kwargs):
+#         return Category.objects.all()
+
+#     def resolve_all_ingredients(self, info, **kwargs):
+#         return Ingredient.objects.all()
+
+#     def resolve_category(self, info, **kwargs):
+#         id = kwargs.get('id')
+#         name = kwargs.get('name')
+
+#         if id is not None:
+#             return Category.objects.get(pk=id)
+
+#         if name is not None:
+#             return Category.objects.get(name=name)
+
+#         return None
+
+#     def resolve_ingredient(self, info, **kwargs):
+#         id = kwargs.get('id')
+#         name = kwargs.get('name')
+
+#         if id is not None:
+#             return Ingredient.objects.get(pk=id)
+
+#         if name is not None:
+#             return Ingredient.objects.get(name=name)
+
+#         return None
+
+
+class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
 
 
-class IngredientType(DjangoObjectType):
+class IngredientNode(DjangoObjectType):
     class Meta:
         model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
 
 
-class Query(object):
-    category = graphene.Field(CategoryType,
-                            id=graphene.Int(),
-                            name=graphene.String())
-    all_categories = graphene.List(CategoryType)
+class Query(graphene.ObjectType):
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
 
-
-    ingredient = graphene.Field(IngredientType,
-                                id=graphene.Int(),
-                                name=graphene.String())
-    all_ingredients = graphene.List(IngredientType)
-
-    def resolve_all_categories(self, info, **kwargs):
-        return Category.objects.all()
-
-    def resolve_all_ingredients(self, info, **kwargs):
-        return Ingredient.objects.all()
-
-    def resolve_category(self, info, **kwargs):
-        id = kwargs.get('id')
-        name = kwargs.get('name')
-
-        if id is not None:
-            return Category.objects.get(pk=id)
-
-        if name is not None:
-            return Category.objects.get(name=name)
-
-        return None
-
-    def resolve_ingredient(self, info, **kwargs):
-        id = kwargs.get('id')
-        name = kwargs.get('name')
-
-        if id is not None:
-            return Ingredient.objects.get(pk=id)
-
-        if name is not None:
-            return Ingredient.objects.get(name=name)
-
-        return None
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
